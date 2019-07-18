08aadbbe45d612c5356e7b7626af97972a08c53b
Wyahid Badri <wyahidb2015@gmail.com>
2019-07-18 16:37:56 -0400

    added page 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Scanner from './components/scanner';
+import RecievingDetail from './components/recievingDetail'
 import './App.css';
 
 function App() {
-  return (
+  return ( 
     <div className="scan-container">
-    <Scanner />
+    {/* <Scanner /> */}
+    <RecievingDetail />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/recievingDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './recievingDetail.css'
+import Scanner from './scanner'
+
+export default function RecievingDetail(){
+
+
+    return(
+        <div className="recieving-detail-main" >
+            <button className="top-button" >top</button>
+            <button className="bottom-button">bottom</button>
+        </div>
+    )
+}
