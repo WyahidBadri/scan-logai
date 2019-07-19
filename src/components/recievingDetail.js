a292075e4721ae85a33f17d4d0dab8384147f8b4
Wyahid Badri <wyahidb2015@gmail.com>
2019-07-19 14:56:22 -0400

    added page 4

--- src/components/recievingDetail.js
+++ src/components/recievingDetail.js
@@ -7,8 +7,10 @@ export default function RecievingDetail(props){
     
     return(
         <div className="recieving-detail-main" onClick=  { () => props.setScan('clicked') }>
-            <button className="top-button" >top</button>
-            <button className="bottom-button">bottom</button>
+            <button className="top-button" ><img className="cam" src='https://cdn1.vectorstock.com/i/1000x1000/86/20/colorful-photo-camera-icon-cartoon-style-vector-15008620.jpg' />
+</button>
+            <button className="bottom-button"><img className="cam" src='https://cdn1.vectorstock.com/i/1000x1000/86/20/colorful-photo-camera-icon-cartoon-style-vector-15008620.jpg' />
+</button>
         </div>
     )
 }
